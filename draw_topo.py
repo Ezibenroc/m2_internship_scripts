4913a35ef3e14b55af3fbae68b78539e275cc318
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-02-22 10:46:45 +0100

    Output to tex/pdf depending on the extension.

--- draw_topo.py
+++ draw_topo.py
@@ -8,8 +8,16 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         print('Syntax: %s <file_name> <topologies>' % sys.argv[0])
         sys.exit(1)
+    filename = sys.argv[1]
     for descriptor in sys.argv[2:]:
         topologies.extend(topology.FatTreeParser.parse(descriptor))
     for topo in topologies:
         topo.initialize()
-    topology.topo_to_pdf(topologies, sys.argv[1])
+    extension = filename.split('.')[-1]
+    if extension == 'pdf':
+        topology.topo_to_pdf(topologies, filename)
+    elif extension == 'tex':
+        topology.topo_to_tex(topologies, filename)
+    else:
+        print('Unknown extension: %s.' % extension)
+        sys.exit(1)
