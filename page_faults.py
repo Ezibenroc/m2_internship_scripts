21a9ef2b5e61ec42ffcd2534e796f15752c19500
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-04-20 15:35:37 +0200

    Several iterations in page_faults script.

--- page_faults.py
+++ page_faults.py
@@ -18,13 +18,14 @@ def measure_page_faults(shared, size, mem_access):
     return minor
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Syntax: %s <max_size> <CSV file>')
+    if len(sys.argv) != 4:
+        print('Syntax: %s <nb_exp> <max_size> <CSV file>')
         sys.exit(1)
-    max_size = int(sys.argv[1])
+    nb_exp = int(sys.argv[1])
+    max_size = int(sys.argv[2])
     sizes = [2**n for n in range(0, floor(log2(max_size))+1)]
-    file_name = sys.argv[2]
-    experiments = list(product([True, False], sizes, [True, False]))
+    file_name = sys.argv[3]
+    experiments = list(product([True, False], sizes, [True, False]))*nb_exp
     random.shuffle(experiments)
     with open(file_name, 'w') as f:
         csv_writer = csv.writer(f)
