ca1b9f60b9c658a2be49c6b98f14905a32204fc7
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-04-20 16:39:21 +0200

    Use a linear scale instead of an exponential scale for sizes.

--- page_faults.py
+++ page_faults.py
@@ -23,16 +23,16 @@ if __name__ == '__main__':
         sys.exit(1)
     nb_exp = int(sys.argv[1])
     max_size = int(sys.argv[2])
-    base = 1.2
-    sizes = [floor(base**n) for n in range(1, floor(log(max_size, base))+1)]
-    sizes = list(sorted(set(sizes)))
     file_name = sys.argv[3]
-    experiments = list(product([True, False], sizes, [True, False]))*nb_exp
-    random.shuffle(experiments)
+    experiments = list(product([True, False], [True, False]))
     with open(file_name, 'w') as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow(('shared', 'size', 'mem_access', 'nb_page_faults'))
-        for i, (shared, size, mem_access) in enumerate(experiments):
-            print('Experiment %d/%d' % ((i+1), len(experiments)))
-            nb_page_faults = measure_page_faults(shared, size, mem_access)
-            csv_writer.writerow((shared, size, mem_access, nb_page_faults))
+        exp_id = 0
+        for exp in range(nb_exp):
+            print('Experiment %d/%d' % (exp+1, nb_exp))
+            random.shuffle(experiments)
+            for shared, mem_access in experiments:
+                size = random.randint(1, max_size)
+                nb_page_faults = measure_page_faults(shared, size, mem_access)
+                csv_writer.writerow((shared, size, mem_access, nb_page_faults))
