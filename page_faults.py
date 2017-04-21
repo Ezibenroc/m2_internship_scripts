9f559fcfc90c2d292c65e1d3bdbd7a5238687593
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-04-21 13:49:38 +0200

    Also collect usr and sys time in page_faults script.

--- page_faults.py
+++ page_faults.py
@@ -9,13 +9,17 @@ from math import log, floor
 from itertools import product
 
 def measure_page_faults(shared, size, mem_access):
-    p = Popen(['/usr/bin/time', '-f', '%F %R', './page_faults', str(int(shared)), str(size), str(int(mem_access))],
+    p = Popen(['/usr/bin/time', '-f', '%S %U %F %R', './page_faults', str(int(shared)), str(size), str(int(mem_access))],
             stdout = DEVNULL, stderr = PIPE)
     output = p.communicate()[1].decode('ascii')
     assert p.wait() == 0
-    major, minor = (int(n) for n in output.split(' '))
+    sys_time, usr_time, major, minor = output.split()
+    sys_time = float(sys_time)
+    usr_time = float(usr_time)
+    major = int(major)
+    minor = int(minor)
     assert major == 0
-    return minor
+    return sys_time, usr_time, minor
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -27,12 +31,12 @@ if __name__ == '__main__':
     experiments = list(product([True, False], [True, False]))
     with open(file_name, 'w') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow(('shared', 'size', 'mem_access', 'nb_page_faults'))
+        csv_writer.writerow(('shared', 'size', 'mem_access', 'system_time', 'user_time', 'nb_page_faults'))
         exp_id = 0
         for exp in range(nb_exp):
             print('Experiment %d/%d' % (exp+1, nb_exp))
             random.shuffle(experiments)
+            size = random.randint(1, max_size)
             for shared, mem_access in experiments:
-                size = random.randint(1, max_size)
-                nb_page_faults = measure_page_faults(shared, size, mem_access)
-                csv_writer.writerow((shared, size, mem_access, nb_page_faults))
+                sys_time, usr_time, nb_page_faults = measure_page_faults(shared, size, mem_access)
+                csv_writer.writerow((shared, size, mem_access, sys_time, usr_time, nb_page_faults))
