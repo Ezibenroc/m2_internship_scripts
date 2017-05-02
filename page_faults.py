75f7d779a335f6d37d2a31bdbdafb6951c968ad4
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-05-02 11:38:40 +0200

    Output CPU utilization in page_faults script.

--- page_faults.py
+++ page_faults.py
@@ -9,17 +9,22 @@ from math import log, floor
 from itertools import product
 
 def measure_page_faults(shared, size, mem_access):
-    p = Popen(['/usr/bin/time', '-f', '%S %U %F %R', './page_faults', str(int(shared)), str(size), str(int(mem_access))],
+    p = Popen(['/usr/bin/time', '-f', '%S %U %F %R %P', './page_faults', str(int(shared)), str(size), str(int(mem_access))],
             stdout = DEVNULL, stderr = PIPE)
     output = p.communicate()[1].decode('ascii')
     assert p.wait() == 0
-    sys_time, usr_time, major, minor = output.split()
+    sys_time, usr_time, major, minor, cpu_percent = output.split()
     sys_time = float(sys_time)
     usr_time = float(usr_time)
     major = int(major)
     minor = int(minor)
+    cpu_utilization = cpu_percent[:-1]
+    if cpu_utilization == '?':
+        cpu_utilization = 'N/A'
+    else:
+        cpu_utilization = float(cpu_utilization)/100
     assert major == 0
-    return sys_time, usr_time, minor
+    return sys_time, usr_time, minor, cpu_utilization
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -31,12 +36,12 @@ if __name__ == '__main__':
     experiments = list(product([True, False], [True, False]))
     with open(file_name, 'w') as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow(('shared', 'size', 'mem_access', 'system_time', 'user_time', 'nb_page_faults'))
+        csv_writer.writerow(('shared', 'size', 'mem_access', 'system_time', 'user_time', 'nb_page_faults', 'cpu_utilization'))
         exp_id = 0
         for exp in range(nb_exp):
             print('Experiment %d/%d' % (exp+1, nb_exp))
             random.shuffle(experiments)
             size = random.randint(1, max_size)
             for shared, mem_access in experiments:
-                sys_time, usr_time, nb_page_faults = measure_page_faults(shared, size, mem_access)
-                csv_writer.writerow((shared, size, mem_access, sys_time, usr_time, nb_page_faults))
+                sys_time, usr_time, nb_page_faults, cpu_utilization = measure_page_faults(shared, size, mem_access)
+                csv_writer.writerow((shared, size, mem_access, sys_time, usr_time, nb_page_faults, cpu_utilization))
