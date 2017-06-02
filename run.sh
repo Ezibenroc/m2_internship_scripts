7c4fd810c25ff1a55618eee324acee6962144963
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-06-02 15:25:09 +0200

    Update.

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 # Example usage, to run a test for N=5000 and nbproc=16: bash run.sh 5000 16
 size=$1
 nb_proc=$2
-./run_measures.py --global_csv result.csv --nb_runs 1 --size ${size} --nb_proc ${nb_proc} --topo "2;16,32;1,16;1,1" --experiment HPL --running_power 5004882812.500 --nb_cpu 8 --hugepage /root/huge
+./run_measures.py --global_csv result.csv --nb_runs 1 --size ${size} --nb_proc ${nb_proc} --topo "2;16,32;1,16;1,1;8" --experiment HPL --running_power 5004882812.500 --hugepage /root/huge
 scp result.csv tocornebize@lyon:/home/tocornebize/result_${size}_${nb_proc}.csv
