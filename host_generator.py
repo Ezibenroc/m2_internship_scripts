7903b60ebf8061f68722336a7c069af89090f027
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-07-28 11:24:21 +0200

    Allow to give a pattern to the host generator.

--- host_generator.py
+++ host_generator.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python3
 import sys
 
-PATTERN = 'host-%d.hawaii.edu'
-
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.stderr.write('Syntax: %s <number of hosts> <filename>\n' % (sys.argv[0]))
+    if len(sys.argv) != 4:
+        sys.stderr.write('Syntax: %s <pattern> <number of hosts> <filename>\n' % (sys.argv[0]))
+        sys.stderr.write('Example of call: {0} "host-%d.stampede.tacc.utexas.edu" 6400 hostnames_stampede.txt\n'.format(sys.argv[0]))
         sys.exit(1)
-    nb_hosts = int(sys.argv[1])
-    with open(sys.argv[2], 'w') as f:
+    nb_hosts = int(sys.argv[2])
+    pattern = sys.argv[1]
+    with open(sys.argv[3], 'w') as f:
         for host_id in range(nb_hosts):
-            f.write(PATTERN % host_id)
+            f.write(pattern % host_id)
             f.write('\n')
